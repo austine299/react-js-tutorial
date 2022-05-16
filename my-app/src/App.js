a738613ee51db8c320d48543957de98b752ddceb
ugochukwu <ugochukwuaugustine299@gmail.com>
2022-05-15 18:36:45 -0700

    my unordered list commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,9 +14,11 @@ function App() {
   )
   return (
     <div className="navbar">
+
       <header>
         <div>{navbar}</div>
       </header>
+      
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
