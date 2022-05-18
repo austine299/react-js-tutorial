0914949754064b95732b02e63f06d270d2088dcb
austine <46765685+austine299@users.noreply.github.com>
2022-05-17 17:47:39 -0700

    Correction (#3)
    * my unordered list commit
    
    * my unordered list commit
    
    * my ordered list commit
    
    * Correction
    
    * Correction

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,21 +4,26 @@ import './App.css';
 function App() {
   const navbar =(
     <nav>
-      <h1>Online Store</h1>
-      <ul>
-        <li>Menu</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+      <h1>Fun facts about react</h1>
+      <ol>
+        <li>Was first released in 2013</li>
+        <li>Was originallytcreated by Jordan Walke</li>
+        <li>Has well over 100k stars on Github </li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobel apps</li>
+      </ol>
     </nav>
   )
+
   return (
     <div className="navbar">
+
+      <img src={logo} className="App-logo" alt="logo" />
+
 
       <header>
         <div>{navbar}</div>
       </header>
-      
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -38,6 +43,7 @@ function App() {
       
     </div>
   );
+  
 }
 
 export default App;
