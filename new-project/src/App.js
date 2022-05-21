a9a4a66d7a34d626044a2a6f694920659864dfbc
ugochukwu <ugochukwuaugustine299@gmail.com>
2022-05-20 18:42:39 -0700

    new project

---
+++ new-project/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './Compoonents/Navbar'
+import Hero from './Compoonents/Hero';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
---
+++ new-project/src/Compoonents/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+import img from "../img/4.png";
+
+export default function Hero(){
+    return(
+        <div>
+            <img src={img}/>
+        </div>
+    )
+}
