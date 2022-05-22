c084207b4141d6ea7feec0d55972c0fed407bdd8
ugochukwu <ugochukwuaugustine299@gmail.com>
2022-05-21 23:32:21 -0700

    creating a digital card

--- new-project/src/Compoonents/Hero.js
+++ new-project/src/Compoonents/Hero.js
@@ -1,10 +1,45 @@
 import React from "react";
-import img from "../img/4.png";
+import Img from "../img/gown3.jpg";
+import Img2 from "../img/gown2.jpg";
+import Img3 from "../img/gown1.jpg";
+import Rate from "../img/star.jpg";
 
 export default function Hero(){
     return(
-        <div>
-            <img src={img}/>
+        <div className="body">
+            <div className="card">
+                <img src={Img} className="card-img" />
+                <div className="crad-stats">
+                    <img src={Rate} className="star"/>
+                    <span>5.0</span>
+                    <span className="gray">(6) .</span>
+                    <span className="gray">Nigeria</span>
+                </div>
+                <p>Sexy Gown</p>
+                <p className="bold">Price: $10</p>
+            </div>
+            <div className="card">
+                <img src={Img2} className="card-img" />
+                <div className="crad-stats">
+                    <img src={Rate} className="star"/>
+                    <span>5.0</span>
+                    <span className="gray">(6) .</span>
+                    <span className="gray">Nigeria</span>
+                </div>
+                <p>Sexy Gown</p>
+                <p className="bold">Price: $10</p>
+            </div>
+            <div className="card">
+                <img src={Img3} className="card-img" />
+                <div className="crad-stats">
+                    <img src={Rate} className="star"/>
+                    <span>5.0</span>
+                    <span className="gray">(6) .</span>
+                    <span className="gray">Nigeria</span>
+                </div>
+                <p>Sexy Gown</p>
+                <p className="bold">Price: $10</p>
+            </div>
         </div>
     )
 }
