473432fb09e857f2034523950675617066d70604
austine <46765685+austine299@users.noreply.github.com>
2022-05-24 13:14:54 +0100

    Props (#7)
    * my unordered list commit
    
    * my unordered list commit
    
    * my ordered list commit
    
    * Correction
    
    * Correction
    
    * commiting my styling
    
    * importing Header, MainBody, and Footer as a function
    
    * importing and exporting function
    
    * new project
    
    * creating a digital card
    
    * using props to make my code reusable
    
    * using props to make my code reuseable
    
    * using props to make my code reuseable

---
+++ new-project/src/Compoonents/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+export default function Card(props){
+    return(
+        <div className="card">
+            <img src={props.img} className="card-img" alt="img" />
+            <div className="crad-stats">
+                <img src={props.rate} className="star" alt="rate"/>
+                <span>{props.num}</span>
+                <span className="gray">{props.dot}</span>
+                <span className="gray">{props.country}</span>
+            </div>
+            <p>{props.cloth}</p>
+            <p className="bold">{props.price}</p>
+        </div>
+    )
+}
+
