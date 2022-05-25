d208b857ea9d335c8664bb8a70522eff3e29b2d7
ugochukwu <ugochukwuaugustine299@gmail.com>
2022-05-25 10:53:46 -0700

    using .MAP to make my coding easy

--- new-project/src/Compoonents/Card.js
+++ new-project/src/Compoonents/Card.js
@@ -5,15 +5,16 @@ import React from "react";
 export default function Card(props){
     return(
         <div className="card">
+            {props.openSpot === 0 && <div className="card-badge">SOLD OUT</div>}
             <img src={props.img} className="card-img" alt="img" />
             <div className="crad-stats">
-                <img src={props.rate} className="star" alt="rate"/>
-                <span>{props.num}</span>
-                <span className="gray">{props.dot}</span>
-                <span className="gray">{props.country}</span>
+                <img src={props.ratingStar} className="star" alt="rate"/>
+                <span >{props.rate}</span>
+                <span>({props.reviewCount}) .</span>
+                <span className="gray"> {props.country}</span>
             </div>
             <p>{props.cloth}</p>
-            <p className="bold">{props.price}</p>
+            <p className="bold">${props.price}</p>
         </div>
     )
 }
---
+++ new-project/src/Compoonents/Data.js
@@ -0,0 +1,144 @@
+export default[
+    {
+        id: 1,
+        coverImg: "../../img/gown3.jpg",
+        ratingStar: "../../img/star.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 7
+        },
+        location: "Online",
+        description: "Sexy gown",
+        price: 50,
+        openSpot: 0,
+    },
+
+    {
+        id: 2,
+        coverImg: "../../img/gown2.jpg",
+        ratingStar: "../../img/star.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 40
+        },
+        location: "Online",
+        description: "Sexy gown",
+        price: 20,
+        openSpot: 23,
+    },
+
+    {
+        id: 3,
+        coverImg: "../../img/gown1.jpg",
+        ratingStar: "../../img/star.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 20
+        },
+        location: "Online",
+        description: "Sexy gown",
+        price: 30,
+        openSpot: 0,
+    },
+
+    {
+        id: 3,
+        coverImg: "../../img/gown1.jpg",
+        ratingStar: "../../img/star.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 20
+        },
+        location: "Online",
+        description: "Sexy gown",
+        price: 30,
+        openSpot: 10,
+    },
+
+    {
+        id: 3,
+        coverImg: "../../img/gown1.jpg",
+        ratingStar: "../../img/star.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 20
+        },
+        location: "Online",
+        description: "Sexy gown",
+        price: 30,
+        openSpot: 33,
+    },
+
+    {
+        id: 3,
+        coverImg: "../../img/gown1.jpg",
+        ratingStar: "../../img/star.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 20
+        },
+        location: "Online",
+        description: "Sexy gown",
+        price: 30,
+        openSpot: 20,
+    }
+]
+
+
+/*
+<div className="card">
+                <img src={Img} className="card-img" alt="img" />
+                <div className="crad-stats">
+                    <img src={Rate} className="star" alt="rate"/>
+                    <span>5.0</span>
+                    <span className="gray">(6) .</span>
+                    <span className="gray">Nigeria</span>
+                </div>
+                <p>Sexy Gown</p>
+                <p className="bold">Price: $10</p>
+            </div>
+            <div className="card">
+                <img src={Img2} className="card-img" alt="img"  />
+                <div className="crad-stats">
+                    <img src={Rate} className="star" alt="rate"/>
+                    <span>5.0</span>
+                    <span className="gray">(6) .</span>
+                    <span className="gray">Nigeria</span>
+                </div>
+                <p>Sexy Gown</p>
+                <p className="bold">Price: $10</p>
+            </div>
+            <div className="card">
+                <img src={Img3} className="card-img" alt="img"  />
+                <div className="crad-stats">
+                    <img src={Rate} className="star" alt="rate"/>
+                    <span>5.0</span>
+                    <span className="gray">(6) .</span>
+                    <span className="gray">Nigeria</span>
+                </div>
+                <p>Sexy Gown</p>
+                <p className="bold">Price: $10</p>
+            </div>
+            <div className="card">
+                <img src={Img} className="card-img" alt="img" />
+                <div className="crad-stats">
+                    <img src={Rate} className="star" alt="rate"/>
+                    <span>5.0</span>
+                    <span className="gray">(6) .</span>
+                    <span className="gray">Nigeria</span>
+                </div>
+                <p>Sexy Gown</p>
+                <p className="bold">Price: $10</p>
+            </div>
+            <div className="card">
+                <img src={Img2} className="card-img" alt="img"  />
+                <div className="crad-stats">
+                    <img src={Rate} className="star" alt="rate"/>
+                    <span>5.0</span>
+                    <span className="gray">(6) .</span>
+                    <span className="gray">Nigeria</span>
+                </div>
+                <p>Sexy Gown</p>
+                <p className="bold">Price: $10</p>
+            </div>
+*/
