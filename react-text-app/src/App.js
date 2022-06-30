b3b1cd6674cb4ac58103b09478f7c6803f0854dd
austine <46765685+austine299@users.noreply.github.com>
2022-07-01 00:23:33 +0100

    Form inputs (#15)
    * passing data to component in react
    
    * passing data to component in react
    
    * making box to be clickable and when on any of the boxes it should change color
    
    * making box to be clickable and when on any of the boxes it should change color
    
    * making use of form input practice and form state object and more

--- react-text-app/src/App.js
+++ react-text-app/src/App.js
@@ -1,25 +1,12 @@
 import React from "react"
 import "./App.css"
-import Box from "./box"
-import boxes from "./Boxes"
+import Form from "./Form";
+
 
 function App(props) {
 
-  const [squares, setSquares] = React.useState(boxes)
-
-
-  const squaresElement  = squares.map(square =>(
-    <Box key={square.id} on = {square.on}/>
-  ))
-
-
   return (
-    <main>
-      
-      {squaresElement}
-
-    </main>
-
+    <Form/>
   );
 }
 
