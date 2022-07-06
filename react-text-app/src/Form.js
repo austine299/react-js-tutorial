99f60e8b661f9267fefa9d125eaff8c4c39d341f
austine <46765685+austine299@users.noreply.github.com>
2022-07-06 23:42:31 +0100

    Sign up form (#16)
    * passing data to component in react
    
    * passing data to component in react
    
    * making box to be clickable and when on any of the boxes it should change color
    
    * making box to be clickable and when on any of the boxes it should change color
    
    * making use of form input practice and form state object and more
    
    * creating a sign up form

--- react-text-app/src/Form.js
+++ react-text-app/src/Form.js
@@ -2,58 +2,88 @@ import React from "react"
 import "./App.css"
 
 export default function Form(){
-    
-    const[formData, setFormData] = React.useState ({firstName: "", lastName: "", emailAddress: "", comment: ""})
+   
+    const[formData, setFormData] = React.useState ({
+        email: "", 
+        password: "",
+        passwordComfirm: "",
+        joinedNewsletter: true
 
+    })
+
+
+
+    function handleachange(event){
+        const{name, value, type, checked} = event.target
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [name]: type === "checkbox" ? checked : value
+        }))
+    }
     console.log(formData)
 
-    function handleChange(event) {
-        setFormData(prevFormData =>{
-            return{
-                ...prevFormData,
-                [event.target.name]: event.target.value
-            }
-        })
+    function handleSubmit(event){
+        event.preventDefault()
+        if(formData.password === formData.passwordComfirm){
+            console.log("successfully signed up")
+        }
+        else{
+            console.log("password do not match")
+            return
+        }
+
+        if(formData.joinedNewsletter){
+            console.log("thank you for signing up for our newsletter")
+        }
     }
-
     
-    return(
-        <form>
-            <input 
-                className="forminput"
-                type="text"
-                placeholder = "First Name"
-                onChange={handleChange}
-                name = "firstName"
-                value={formData.firstName}
-            />
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit} className ="form">
+                <input 
+                    className="forminput"
+                    type="email"
+                    placeholder = "Email address"
+                    name = "email"
+                    onChange={handleachange}
+                    value={formData.email}
+                />
 
-            <input
-                className="forminput" 
-                type="text"
-                placeholder = "First Name"
-                onChange={handleChange}
-                name = "lastName"
-                value={formData.lastName}
-            />
-            <input 
-                className="forminput"
-                type="text"
-                placeholder = "Email Address"
-                onChange={handleChange}
-                name = "emailAddress"
-                value={formData.emailAddress}
+                <input
+                    className="forminput"
+                    type="password"
+                    placeholder = "*******"
+                    name = "password"
+                    onChange={handleachange}
+                    value={formData.password}
+                />
+
+                <input
+                    className="forminput"
+                    type="password"
+                    placeholder = "Comfirm password"
+                    name = "passwordComfirm"
+                    onChange={handleachange}
+                    value={formData.passwordComfirm}
+                />
                 
-            />
+                <br />
+                <div className="form--marketing">
+                <input
+                    id="okayToEmail"
+                    type="checkbox"
+                    placeholder = "Comfirm password"
+                    name = "joinedNewsletter"
+                    onChange={handleachange}
+                    checked={formData.joinedNewsletter}
+                />
+                <label htmlFor="okayToEmail"> I want join newsletter</label>
+                </div>
+                
+                <br />
+                <button className="btn ">Sign Up</button>
+            </form>
+        </div>
 
-            <textarea 
-                className="forminput" 
-                placeholder="whatever you put inside"
-                onChange={handleChange}
-                name="comment"
-                value={formData.comment}
-            
-            /> 
-        </form>
     )
 }
